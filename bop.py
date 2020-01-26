8f0fa28b5f4c3a3adfff4cd97b7645becf3efd35
SouravBera <53810519+SouravBear8850@users.noreply.github.com>
2020-01-26 13:38:01 +0530

    Add bop

---
+++ bop.py
@@ -0,0 +1,36 @@
+from telegram.ext import Updater, CommandHandler
+import requests
+import re
+
+
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()
+    image_url = contents['url']
+    return image_url
+
+
+def get_image_url():
+    allowed_extension = ['jpg', 'jpeg', 'png']
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_url()
+        file_extension = re.search("([^.]*)$", url).group(1).lower()
+    return url
+
+
+def bop(bot, update):
+    url = get_image_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+
+
+def main():
+    updater = Updater('1027621269:AAH0ldH-TZ-ctYpvXSQC3aybSm1L4RyiK0Q', use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('bop', bop))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
